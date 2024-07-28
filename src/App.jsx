3ff7e206a40bf237ccde25d104b426c9d5c37fd0
starfart69 <joshuel.simbulan@gmail.com>
2024-07-28 16:50:29 +0800

    before adding timeline

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,28 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="myStack">
+      {/* <div className="myStack">
         <h1>My Stack</h1>
         <Stack />
         <Stack />
         <Stack />
+      </div> */}
+      <div className="aboutMe">
+        <div className="aboutMeText">
+          <h1>Hello!</h1>
+          <p>My name is <span className='underline'>Joshuel Ernest Simbulan.</span> I make apps.</p>
+        </div>
+        <div className="spotifyPlaylist">
+          <iframe 
+            src="https://open.spotify.com/embed/playlist/2IkrNzfNGZBRYP6WbVVRPz?utm_source=generator&theme=0" 
+            width="100%" 
+            allowFullScreen 
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            loading="lazy">
+          </iframe>
+        </div>
       </div>
     </>
   )
 }
-
 export default App
